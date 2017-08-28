d5d8d5a41c5b22ddf47ec11e4209bbe3158093d0
Bram Vandenbogaerde <bram.vandenbogaerde@gmail.com>
2017-08-28 20:02:48 +0200

    added tests for literals beyond the symbol literal

--- src/tests.rs
+++ src/tests.rs
@@ -10,10 +10,6 @@ fn nested_list(){
     assert_eq!(parse("((set!))").car().car().unwrap_symbol(), "set!")
 }
 
-#[test]
-fn literals(){
-    assert_eq!(parse("(if 'symb)").cdr().car().unwrap_literal(), "symb")
-}
 
 #[test]
 fn numbers(){
@@ -40,6 +36,24 @@ fn bool_literal_case_insentive(){
 fn true_oposite_of_false(){
     assert_eq!(parse("(#t)").car().is_false(), false);
     assert_eq!(parse("(#f)").car().is_false(), true);
+}
+
+// Test for literals, to make sure that no parsing is done after a ' symbol
+
+#[test]
+fn list_literals(){
+    assert_eq!(parse("('(1 2 3))").car().unwrap_literal(), "(1 2 3)")
+}
+
+#[test]
+fn nested_list_literals(){
+    assert_eq!(parse("('((1 2) 3))").car().unwrap_literal(), "((1 2) 3)")
+}
+
+
+#[test]
+fn literals(){
+    assert_eq!(parse("(if 'symb)").cdr().car().unwrap_literal(), "symb")
 }
 
 #[test]
